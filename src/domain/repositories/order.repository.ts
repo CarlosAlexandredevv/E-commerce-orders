4b5cd22ad94e25736404f57aabe0f71e895cf086
Carlos Alexandre <alexfilho062005@gmail.com>
2025-05-01 16:15:45 -0300

    :sparkles: feat: add findAllWithFilters in IOrderRepository

--- src/domain/repositories/order.repository.ts
+++ src/domain/repositories/order.repository.ts
@@ -4,4 +4,10 @@ export interface IOrderRepository {
   create(order: Partial<OrderEntity>): OrderEntity;
   save(order: OrderEntity): Promise<OrderEntity>;
   findOneById(id: string): Promise<OrderEntity | null>;
+
+  findAllWithFilters(filters: {
+    status?: string;
+    startDate?: string;
+    endDate?: string;
+  }): Promise<OrderEntity[]>;
 }
