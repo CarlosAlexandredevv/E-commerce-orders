7ca79b7164ab7159f26fa457370d2cbe4edb6549
Carlos Alexandre <alexfilho062005@gmail.com>
2025-05-01 00:44:32 -0300

    :heavy_plus_sign: chore: add bull and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
-// filepath: src/app.module.ts
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { OrderModule } from './interfaces/controllers/order.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(typeOrmConfig),
     OrderModule,
+    BullModule.forRoot({
+      redis: { host: 'localhost', port: 6379 },
+    }),
+    BullModule.registerQueue({ name: 'order' }),
   ],
 })
 export class AppModule {}
